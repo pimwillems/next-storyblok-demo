fdf492a29c4e947104a7ad8df9a7c2822f73c3ea
Pim Willems <git.boxer492@aleeas.com>
2024-09-09 23:15:30 +0200

    Init

---
+++ components/StoryblokProvider.ts
@@ -0,0 +1,30 @@
+"use client";
+
+import {
+  storyblokInit,
+  apiPlugin,
+  SbReactComponentsMap,
+} from "@storyblok/react/rsc";
+import Page from "./page.component";
+import Feature from "./Feature";
+
+const components = {
+  page: Page,
+  feature: Feature,
+};
+
+storyblokInit({
+  accessToken: process.env.NEXT_PUBLIC_STORYBLOK_ACCESS_TOKEN,
+  use: [apiPlugin],
+  components: components as SbReactComponentsMap,
+});
+
+export default function StoryblokProvider({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return children;
+}
+
+console.log(process.env.NEXT_PUBLIC_STORYBLOK_ACCESS_TOKEN);
